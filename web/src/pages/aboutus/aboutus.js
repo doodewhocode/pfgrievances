109fa99ab70207ebcf25c75f83ed8bb0e198c7a6
vivekvppanneerselvam <vivekvppanneerselvam@gmail.com>
2021-11-08 23:03:16 +0530

    framework setup

---
+++ web/src/pages/aboutus/aboutus.js
@@ -0,0 +1,68 @@
+import React, {useEffect} from 'react'
+
+function AboutUs(){
+return(
+
+    <div class="section-gap" style="margin-left: 1.5rem;">
+       
+        <div class="about-us" id="services" class="section">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-12 text-center mt-5">
+                        <div class="title" style="padding: 0 150px;">We're putting
+                            imagination to work</div>
+                        <div class="description text-center mt-3" style="padding: 0 150px;">Comply HR incorporated in
+                            2021.
+                            ComplyHR is one stop shop, data
+                            driven E compliance cloud based
+                            Platform, for your company’s
+                            compliance requirements, HR Polices
+                            ,Auto labour law registers generations
+                            ,Live chat & other Compliance
+                            solutions across the organizations
+                            varying from different Business
+                            segments under different labor
+                            acts
+                        </div>
+                    </div>
+                    <div class="col-12 ">
+                        <div class="about-img mt-5">
+                            <img src="./assets/pexels-fauxels-3184418.jpg"/>
+                        </div>
+                    </div>
+                    <div class="col-md-12 mt-5">
+                        <div class="title">Lorem ipsum dolor</div>
+                    </div>
+                    <div class="col-md-6 mt-5">
+                        <div class="description text-justify">Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+                            Consectetur,
+                            hic? Delectus provident asperiores accusamus sapiente nostrum eos voluptas cupiditate atque,
+                            dolorem nihil corporis, quisquam maiores aperiam ipsum animi, nemo adipisci.voluptas
+                            cupiditate atque,
+                            dolorem nihil corporis, quisquam maiores aperiam ipsum animi, nemo adipisci.voluptas
+                            cupiditate atque,
+                            dolorem nihil corporis, quisquam maiores aperiam ipsum animi, nemo adipisci.voluptas
+                            cupiditate atque,
+                            dolorem nihil corporis, quisquam maiores aperiam ipsum animi, nemo adipisci.</div>
+                    </div>
+                    <div class="col-md-6 mt-5">
+                        <div class="description text-justify">Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+                            Consectetur,
+                            hic? Delectus provident asperiores accusamus sapiente nostrum eos voluptas cupiditate atque,
+                            dolorem nihil corporis, quisquam maiores aperiam ipsum animi, nemo adipisci.voluptas
+                            cupiditate atque,
+                            dolorem nihil corporis, quisquam maiores aperiam ipsum animi, nemo adipisci.voluptas
+                            cupiditate atque,
+                            dolorem nihil corporis, quisquam maiores aperiam ipsum animi, nemo adipisci.voluptas
+                            cupiditate atque,
+                            dolorem nihil corporis, quisquam maiores aperiam ipsum animi, nemo adipisci.</div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+	)
+	}
+	
+	export default AboutUs
+   
